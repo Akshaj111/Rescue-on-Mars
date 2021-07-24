03f0a8ebed63e7652cefaafdaa2c175cf42d24f7
Akshaj111 <76700912+Akshaj111@users.noreply.github.com>
2021-07-24 18:16:32 +0100

    Add files via upload

---
+++ form.js
@@ -0,0 +1,10 @@
+class form{
+constructor(){
+    this.button = createButton("PLAY!"); 
+}
+display(){
+    this.button.position(WindowWidth/2, WindowHeight-100);
+}
+
+
+};
---
+++ sketch.js
@@ -0,0 +1,12 @@
+var backG;
+var play;
+function preload(){
+backG = loadImage("backGMarsImg.jpg");
+}
+function setup(){
+createCanvas(windowWidth,windowHeight)
+}
+function draw (){
+background(backG);
+
+}
